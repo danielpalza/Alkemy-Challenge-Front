0fd4f5f07c902cc9af4249e679483dfeadca8af8
danielpalza <danielpalza@hotmail.com>
2021-02-04 13:09:36 -0300

    Login style

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,12 +1,11 @@
 import Fetch from "../services/Fetch";
-import { Redirect } from "react-router-dom";
 import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Login(p) {
-    
   const [user, setUser] = useState({ email: "", password: "" });
 
-  const handleSubmit = (e) => {
+  const handleLogin = (e) => {
     Fetch("POST", "/usuario/login", user, "", handleResponse);
   };
   const handleChange = (e) => {
@@ -14,29 +13,51 @@ export default function Login(p) {
   };
   const handleResponse = (e) => {
     localStorage.setItem("token", e.response.token);
-
     if (e.status === "ok") {
       p.isLogin(true);
     }
   };
 
   return (
-    <div>
-      <form className="bg-green-300 flex justify-around p-4">
-        <input
-          name="email"
-          placeholder="Email..."
-          onChange={handleChange}
-          type="text"
-        />
-        <input
-          name="password"
-          placeholder="Contraseña..."
-          onChange={handleChange}
-          type="password"
-        />
-        <input type="button" onClick={handleSubmit} value="Enviar" />
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <div className="flex justify-center h-screen items-center">
+            <form className="rounded w-1/4 bg-green-300 flex flex-col p-10">
+              <input
+                name="email"
+                placeholder="Email..."
+                onChange={handleChange}
+                type="text"
+                className="p-2  m-10"
+              />
+              <input
+                name="password"
+                placeholder="Contraseña..."
+                onChange={handleChange}
+                type="password"
+                className="m-10 p-2"
+              />
+              <div className="flex flex-col justify-center items-center">
+                <button
+                  onClick={handleLogin}
+                  className="bg-green-300 p-3 m-5 rounded-md  text-green-900 transform shadow-lg hover:bg-green-400 hover:scale-110 duration-200"
+                >
+                  Enviar
+                </button>
+                <Link to="/register">
+                  <button className="bg-green-300 p-3 rounded-md  text-green-900 transform shadow-lg hover:bg-green-400 hover:scale-110 duration-200">
+                    Registrarse
+                  </button>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </Route>
+        <Route path="/register">
+          <h1>Registro</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
